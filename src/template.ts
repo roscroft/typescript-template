520083fb35bb85df87b5c17b9b1cd5f31860354a
Austin Herrling <adherrling@gmail.com>
2023-04-08 12:13:11 -0500

    Update packages and src testing

--- src/template.ts
+++ src/template.ts
@@ -3,3 +3,7 @@ import { NS } from "@ns";
 export async function main(ns: NS): Promise<void> {
   ns.tprint("Hello Remote API!");
 }
+
+export async function testing(ns: NS): Promise<void> {
+  ns.tprint("Testing!")
+}
